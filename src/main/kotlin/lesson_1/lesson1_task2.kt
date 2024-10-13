23da0348b5fcefb1a91a08c488a6a2ed82dea53c
evgeny4325 <4325@mail.ru>
2024-10-13 20:53:31 +0300

    KS-1-2 new answer Workers

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,5 +1,15 @@
 package org.example.lesson_1
 
 fun main() {
-    println("урок 1 задача 2")
+
+    val newOrderNumber: Int = 75
+    val thankYou: String = "Спасибо за покупку!"
+
+    println("Номер вашего заказа $newOrderNumber. $thankYou")
+
+    val numberOfWorkers: Int = 2000
+    val numberOfDismissed: Int = 1
+
+    println("В нашем магазине для Вас работают ${numberOfWorkers-numberOfDismissed} сотрудников")
+
 }
